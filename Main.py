bbf00ae841298c2df385d334b1c46d061d8ea4df
Bzaigege <Bzaixiaozhanxin@163.com>
2019-01-29 13:59:53 +0800

    自动化打包工具更新至2.0.0版本
    1、重构优化代码、目录结构
    2、修复适配bug,包括不限于apk版本适配、aapt版本适配，资源及资源属性冲突、丢失等问题修复;第三方库适配多和R文件问题等等
    3、新增十多家渠道适配代码
    4、新增桌面UI打包界面
    5、将示例资源抽离,方便查看源码结构
    
    等等。后续还会有完整的打包系统博客更新。

--- Main.py
+++ Main.py
@@ -13,8 +13,11 @@ def startBuildApkTask():
     # gameId                       游戏ID
     # gameVersion                  游戏版本
     # gameApkName                  游戏母包名称
+    # channelName                  渠道名称
     # channelId                    渠道ID
     # channelVersion               渠道版本
+    # isLocal                      是否是本地打包(区分服务器打包,主要处理差异化配置)
+    # signId                       签名文件ID，默认为0, 本地桌面打包默认设置为1
     # keystore                     签名文件名称,为默认
     # alias                        签名文件别名，为默认
     # storepass                    签名文件密码，为默认
